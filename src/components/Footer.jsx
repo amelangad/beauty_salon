024aa0342739716352b0571206c0d9f458e3055f
amelangad <magdalena.slodzinska@gmail.com>
2023-04-11 20:45:33 +0200

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/footer.css'
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export default function Footer() {
 
@@ -15,11 +16,15 @@ const scrollUp = () => {
 <button className ="footer__button"
 onClick={scrollUp}>UP</button>
 <div className ="footer__info">
-    <div className ="footer__adress"></div>
-    <div className ="footer__adress"></div>
-    <div className ="footer__adress"></div>
+    <div className ="footer__adress">ul. Konwaliowa 8, Poznań<br/><span className ="footer__phone">tel. 555 222 115</span></div>
+  <hr/>
+    <div className ="footer__media">
+<hr></hr>
+    <div className ="footer__media--item"><FaFacebook/></div>
+    <div className ="footer__media--item"><FaInstagram/></div>
+    <div className ="footer__media--item"><FaYoutube/></div>
+    </div>
 </div>
-
     </div>
   )
 }
